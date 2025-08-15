a4868b17d2d45c1d79e8d0c34c0e38dca7f95dfc
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-08-15 18:56:41 -0300

    refactor: tools/github_reader.py

--- tools/github_reader.py
+++ tools/github_reader.py
@@ -1,11 +1,11 @@
 import re
 from github import Github
 from github.Auth import Token
-from google.colab import userdata
+import os
 
-def conection(repositorio: str):
-    GITHUB_TOKEN = userdata.get('github_token')
-    auth = Token(GITHUB_TOKEN)
+
+def conection(repositorio: str, github_token: str):
+    auth = Token(github_token)
     g = Github(auth=auth)
     return g.get_repo(repositorio)
 
@@ -15,8 +15,9 @@ MAPEAMENTO_TIPO_EXTENSOES = {
     "python": [".py"],
     "cloudformation": [".json", ".yaml", ".yml"],
     "ansible": [".yml", ".yaml"],
-    "docker": ["Dockerfile"], 
+    "docker": ["Dockerfile"],
 }
+
 
 def _leitura_recursiva_com_debug(repo, extensoes, path="", arquivos_do_repo=None):
 
@@ -24,21 +25,19 @@ def _leitura_recursiva_com_debug(repo, extensoes, path="", arquivos_do_repo=None
         arquivos_do_repo = {}
 
     try:
-        # Tentando obter o conteúdo do caminho
         conteudos = repo.get_contents(path)
 
         for conteudo in conteudos:
             if conteudo.type == "dir":
                 _leitura_recursiva_com_debug(repo, extensoes, conteudo.path, arquivos_do_repo)
             else:
-                # Lógica de decisão de leitura
                 ler_o_arquivo = False
                 if extensoes is None:
                     ler_o_arquivo = True
                 else:
                     if any(conteudo.path.endswith(ext) for ext in extensoes) or conteudo.name in extensoes:
                         ler_o_arquivo = True
-                    
+
                 if ler_o_arquivo:
                     try:
                         codigo = conteudo.decoded_content.decode('utf-8')
@@ -48,17 +47,17 @@ def _leitura_recursiva_com_debug(repo, extensoes, path="", arquivos_do_repo=None
 
     except Exception as e:
         print(e)
-        
+
     return arquivos_do_repo
 
 
-def main(repo, tipo_de_analise: str):
+def main(repo, tipo_de_analise: str, github_token: str):
 
-    repositorio_final = conection(repositorio=repo)
+    repositorio_final = conection(repositorio=repo, github_token=github_token)
 
     extensoes_alvo = MAPEAMENTO_TIPO_EXTENSOES.get(tipo_de_analise.lower())
 
-    arquivos_encontrados = _leitura_recursiva_com_debug(repositorio_final, 
+    arquivos_encontrados = _leitura_recursiva_com_debug(repositorio_final,
                                                         extensoes=extensoes_alvo)
-  
+
     return arquivos_encontrados
